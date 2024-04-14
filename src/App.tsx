e0a68dd08ff155cfcaa5b20be987321e4b284925
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-15 00:31:35 +0800

    inputes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [emojis, updateEmoji] = useState<any>(undefined);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-
-  const [ keys, setKey ] = useState<string[] | null>(null);
+  const [ emojis, updateEmojis ] = useState<string[] | null>(null);
+  const [ value, setValue ] = useState('');
 
   useEffect(() => {
     const fetchEmojis = async () => {
@@ -16,8 +15,7 @@ export default function App() {
           throw new Error('Failed to fetch emojis');
         }
         const data = await response.json();
-        updateEmoji(data);
-        setKey(Object.keys(data));
+        updateEmojis(Object.values(data));
       } catch (error:any) {
         setError(error.message || 'Failed to fetch emojis');
       } finally {
@@ -27,8 +25,6 @@ export default function App() {
 
     fetchEmojis();
   }, []);
-
-  console.log(keys)
 
   if (loading) {
     return <div>Loading...</div>;
@@ -41,8 +37,10 @@ export default function App() {
   return (
     <div className="Home">
       <h1>Explore All Github Emojis!!</h1>
-      <div>Hello</div>
-      {emojis && emojis.accept && <img src={emojis.accept} alt="Accept Emoji" />}
+      <input type="text" onChange={e => setValue(e.target.value)} value={value}/>
+      <div className='display'>
+        
+      </div>
     </div>
   );
 }
