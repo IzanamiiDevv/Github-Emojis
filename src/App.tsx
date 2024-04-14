c9414b9790c8c12188b0c1a26d032eb9e3c994b9
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-15 00:12:11 +0800

    script

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [emojis, updateEmoji] = useState<any>(undefined);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const [ keys, setKey ] = useState<string[] | null>(null);
+
+  useEffect(() => {
+    const fetchEmojis = async () => {
+      try {
+        const response = await fetch('https://api.github.com/emojis');
+        if (!response.ok) {
+          throw new Error('Failed to fetch emojis');
+        }
+        const data = await response.json();
+        updateEmoji(data);
+        setKey(Object.keys(data));
+      } catch (error:any) {
+        setError(error.message || 'Failed to fetch emojis');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEmojis();
+  }, []);
+
+  console.log(keys)
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="Home">
+      <h1>Explore All Github Emojis!!</h1>
+      <div>Hello</div>
+      {emojis && emojis.accept && <img src={emojis.accept} alt="Accept Emoji" />}
+    </div>
+  );
 }
-
-export default App
